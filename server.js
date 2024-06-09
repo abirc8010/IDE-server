44834b52425fff910dd5f9f111d863c6def23c6f
abirc8010 <abirc8010@gmail.com>
2024-06-09 18:30:42 +0530

    Added server code

---
+++ server.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+const versionIndices = {
+  python: "0",
+  java: "0",
+  c_cpp: "0",
+  golang: "0"
+};
+
+app.post('/api/run-code', async (req, res) => {
+  let { code, input, language } = req.body;
+
+  const versionIndex = versionIndices[language] || "0"; 
+  language=(language==="c_cpp")?"cpp17":(language==="java")?"java":(language==="python")?"python3":"go";
+  try {
+    const response = await axios.post('https://api.jdoodle.com/v1/execute', {
+      clientId: process.env.JDOODLE_CLIENT_ID,
+      clientSecret: process.env.JDOODLE_CLIENT_SECRET,
+
+      script: code,
+      language: language,
+      versionIndex: versionIndex,
+      stdin: input,
+    });
+    console.log(response.data);
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error running code:', error);
+    res.status(500).json({ error: 'Error running code' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
